f1ef707465b47e6a54444e09f8f25a7bf88e0cd8
Uniq <nvalerosanchezdeveloper@gmail.com>
2026-04-14 17:09:26 +0200

    Update ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -10,7 +10,7 @@ number_1 = input("Introduzca el primer número: ")
 number_2 = input("Introduzca el segundo número: ")
 
 number_1_to_int = int(number_1)
-number_2_to = int(number_2)
+number_2_to_int = int(number_2)
 
 if number_1_to_int %2 == 0:
     print("El número es par")
