d9993d91f2b621950ea5178acd5fcff804160fa9
Uniq <nvalerosanchezdeveloper@gmail.com>
2026-04-14 17:07:16 +0200

    Update ejercicio_2.py

---
+++ ejercicio_2.py
@@ -0,0 +1,18 @@
+# Ejercicio 2
+
+
+# Escribe un programa que pida al usuario dos números (usa input() y conviértelos a int).
+# Muestra todos los resultados aritméticos: suma, resta, multiplicación, división, división entera, módulo y potencia.
+# Indica si el primer número es par o impar (pista: usa %).
+
+
+number_1 = input("Introduzca el primer número: ")
+number_2 = input("Introduzca el segundo número: ")
+
+number_1_to_int = int(number_1)
+number_2_to = int(number_2)
+
+if number_1_to_int %2 == 0:
+    print("El número es par")
+else: print("El número es impar")
+
